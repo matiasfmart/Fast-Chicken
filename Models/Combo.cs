8d2301e23efc4b002ac182c8e5b31e5c52a9998c
Matias Martinez <martinez_matias@live.con.ar>
2022-10-26 16:13:01 -0300

    maquetado semi-terminado

--- Models/Combo.cs
+++ Models/Combo.cs
@@ -3,7 +3,9 @@
     public class Combo
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public float Price { get; set; }
+        public string Description { get; set; }
         public Product Main { get; set; }
         public Product Side { get; set; }
         public Product Drink { get; set; }
--- Models/Order.cs
+++ Models/Order.cs
@@ -5,8 +5,13 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int OrderNum { get; set; }
+        public List<Combo> Combos { get; set; }
         public float Price { get; set; }
         public DateTime Date { get; set; }
 
+        public Order()
+        {
+            this.Combos = new List<Combo>();
+        }
     }
 }
